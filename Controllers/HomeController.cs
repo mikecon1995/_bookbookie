85909b6faa245600b64ebf7a6d735ca795e4a63d
mikecon1995 <mikecon1995gmail.com>
2021-03-27 16:38:12 -0400

    added mvc to application

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace bookbookie.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
         public string Idex()
         {
