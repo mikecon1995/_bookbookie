05481f00e8d8fd1a3254136ef3c5e28c4d0c034e
mikecon1995 <mikecon1995gmail.com>
2021-03-27 17:27:08 -0400

    Book Controller / Model and repository

---
+++ Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bookbookie.Repository;
+using bookbookie.Models;
+
+namespace bookbookie.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRespository _bookRepository = null;
+
+        public BookController()
+        {
+            _bookRepository = new BookRespository();
+        }
+
+       public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetallBooks();
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBooks(String bookName, String authorName)
+        {
+            return _bookRepository.SearchBook(bookName, authorName);
+        }
+    }
+}
---
+++ Repository/BookRespository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using bookbookie.Models;
+
+namespace bookbookie.Repository
+{
+    public class BookRespository
+    {
+        public List<BookModel> GetallBooks()
+        {
+
+        }
+        public BookModel GetBookById(int id)
+        {
+            return DataSource().Where(x => x.Id == id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(String title, String authorName)
+        {
+            return DataSource().Where(x => x.Title == title && x.Author == authorName).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id = 1, Title = "MVC", Author = "Mike"},
+                new BookModel(){Id = 2, Title = "MVC", Author = "Mike"},
+                new BookModel(){Id = 3, Title = "C#", Author = "Mike"},
+                new BookModel(){Id = 4, Title = "Java", Author = "Mike"},
+                new BookModel(){Id = 5, Title = "PHP", Author = "Mike"},
+            };
+        }
+    }
+}
