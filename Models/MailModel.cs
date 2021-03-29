b956e86b51c4a3a530360746f75f857dc262dd6d
mikecon1995 <mikecon1995gmail.com>
2021-03-28 20:33:09 -0400

    added email functionality

---
+++ Models/MailModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookbookie.Models
+{
+    public class MailModel : Controller
+    {
+        public string To { get; set; }
+        public string From { get; set; }
+
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
